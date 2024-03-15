760ed5faa4f2ece03dabf9f2ae3f3d7342e2b95f
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-03-15 12:31:14 +0100

    Adujst SingleTableForm to TableForm

--- src/components/features/SingleTableForm/SingleTableForm.js
+++ src/components/features/SingleTableForm/SingleTableForm.js
@@ -1,48 +1,18 @@
-import { Button, Form } from "react-bootstrap";
 import { useDispatch, useSelector} from "react-redux";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { editTableRequest, getTableById } from "../../../redux/tablesRedux";
-import { getAllStatuses } from "../../../redux/tableStatusesRedux";
-import style from './SingleTableForm.module.scss';
-import clsx from 'clsx';
-import { useState } from "react";
-import adjustAmountPeople from "../../../utils/adjustAmountPeople";
+import TableForm from "../TableForm/TableForm";
 
 
 const SingleTableForm = () => {
   const {id} = useParams();
   const table = useSelector(state => getTableById(state, id));
-  const allStatuses = useSelector(getAllStatuses);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const [status, setStatus] = useState(table.status || '');
-  const [peopleAmount, setPeopleAmount] = useState(table.peopleAmount || '');
-  const [maxPeopleAmount, setMaxPeopleAmount] = useState(table.maxPeopleAmount || '');
-  const [bill, setBill] = useState(table.bill || '');
-  const [showBill, setShowBill] = useState(status === 'Busy');
-
-  const selectChange = newStatus =>{
-    setStatus(newStatus);
-    if (newStatus === 'Busy'){
-      setShowBill(true);
-      
-    } else {
-      setShowBill(false);
-      setBill(0);
-    }
-  };
-  
-  const submitHandle = e => {
-    e.preventDefault();
-    const table = {
-      id: id,
-      status: status,
-      peopleAmount: parseInt(peopleAmount),
-      maxPeopleAmount: parseInt(maxPeopleAmount),
-      bill: parseInt(bill)
-    };
-    dispatch( editTableRequest(table));
+  const submitHandle = updatedTable => {
+    dispatch( editTableRequest({id, ...updatedTable}));
     navigate('/');
   };
 
@@ -50,25 +20,9 @@ const SingleTableForm = () => {
   else return(
     <div>
       <h2>Table {id}</h2>
-      <Form onSubmit={e => submitHandle(e)}>
-          <Form.Group className="my-2">
-            <Form.Label className="fw-bold">Status: </Form.Label>
-            <Form.Select value={status} onChange={ e => selectChange(e.target.value)} className={style.select} >
-                { allStatuses.map(status => <option key={status.id} value={status.status}> {status.status} </option>) }
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="my-2">
-            <Form.Label className="fw-bold" >People: </Form.Label>
-            <Form.Control value={peopleAmount} onChange={e => setPeopleAmount(adjustAmountPeople(e.target.value, maxPeopleAmount))} className={clsx(style.inputPeople, "me-2")} type="number" ></Form.Control>
-            /
-            <Form.Control value={maxPeopleAmount} onChange={e=> setMaxPeopleAmount(adjustAmountPeople(e.target.value))} className={clsx(style.inputPeople, "ms-2")} type="number" ></Form.Control>
-          </Form.Group >
-          <Form.Group className={clsx( !showBill && style.isShowBill, "my-2")}>
-            <Form.Label className="fw-bold" >Bill: </Form.Label>
-            $<Form.Control value={bill} onChange={e => setBill(e.target.value)} className={clsx(style.inputBill, "ms-2")} type="number" ></Form.Control>
-          </Form.Group>
-        <Button type="submit"  className="mt-2">Update</Button>
-      </Form>
+      <TableForm action={submitHandle} actionText={'Update'} 
+      status={table.status} peopleAmount={table.peopleAmount} 
+      maxPeopleAmount={table.maxPeopleAmount} bill={table.bill} />
     </div>
   )
 };
